0b2e3f700bcbd9539797673c317457cef2d915e8
Andrey <dolby0@gmail.com>
2015-06-23 16:45:16 +0700

    Production port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ build.run(function(err, stats){
   if (err) { throw err }
   console.log('- build complete')
 
-  var server = app.listen(3000, function () {
+  var server = app.listen(process.env.PORT || 3000, function () {
 
     var host = server.address().address
     var port = server.address().port
